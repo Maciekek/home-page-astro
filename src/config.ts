03ad090ace7ef37e86adb01c637c8aa5ef155b3b
maciej.kucharski <maciej.kucharski@senti1.com>
2024-03-14 15:55:36 +0100

    og meta tags update

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+const config = {
+  email: "maciekek@gmail.com",
+  title: "Maciej Kucharski",
+  description: "Developer, passionate about web technologies and programming.",
+  tagline: "Personal page of Maciej Kucharski",
+};
+
+export { config };
